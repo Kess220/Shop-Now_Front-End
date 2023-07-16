98b27e9facb0b25964a03e67bb8a87cad54224ac
unknown <prodvictorkaio@gmail.com>
2023-07-16 12:42:08 -0300

    homepage

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,6 @@ import styled from "styled-components";
 import HomePage from "./pages/HomePage";
 import SignInPage from "./pages/SignInPage";
 import SignUpPage from "./pages/SignUpPage";
-import TransactionsPage from "./pages/TransactionPage";
 
 export default function App() {
   return (
@@ -13,7 +12,6 @@ export default function App() {
           <Route path="/" element={<SignInPage />} />
           <Route path="/cadastro" element={<SignUpPage />} />
           <Route path="/home" element={<HomePage />} />
-          <Route path="/nova-transacao/:tipo" element={<TransactionsPage />} />
         </Routes>
       </BrowserRouter>
     </PagesContainer>
